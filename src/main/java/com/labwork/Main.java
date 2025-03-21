f64f7b86ed322832859cc7d24369508a37f50fc9
crinqoliq <lomaniinoss@yandex.ru>
2025-03-21 12:20:44 +0300

    quick fix

--- src/main/java/com/labwork/Main.java
+++ src/main/java/com/labwork/Main.java
@@ -25,17 +25,17 @@ public class Main {
 
 
         CsvWriter csvWriter = new CsvWriter();
-        csvWriter.writeResults("sin.scv", -5, 5, sin, 0.1);
-        csvWriter.writeResults("cos.scv", -5, 5, cos, 0.1);
-        csvWriter.writeResults("sec.scv", -5, 5, sec, 0.1);
-        csvWriter.writeResults("cot.scv", -5, 5, cot, 0.1);
-        csvWriter.writeResults("tan.scv", -5, 5, tan, 0.1);
-        csvWriter.writeResults("csc.scv", -5, 5, csc, 0.1);
+        csvWriter.writeResults("sin.csv", -5, 5, sin, 0.1);
+        csvWriter.writeResults("cos.csv", -5, 5, cos, 0.1);
+        csvWriter.writeResults("sec.csv", -5, 5, sec, 0.1);
+        csvWriter.writeResults("cot.csv", -5, 5, cot, 0.1);
+        csvWriter.writeResults("tan.csv", -5, 5, tan, 0.1);
+        csvWriter.writeResults("csc.csv", -5, 5, csc, 0.1);
 
 
-        csvWriter.writeResults("ln.scv", 0.00001, 5, ln, 0.1);
-        csvWriter.writeResults("log2.scv", 0.00001, 5, log2, 0.1);
-        csvWriter.writeResults("log5.scv", 0.00001, 5, log5, 0.1);
-        csvWriter.writeResults("system.scv", -5, 5, function, 0.1);
+        csvWriter.writeResults("ln.csv", 0.00001, 5, ln, 0.1);
+        csvWriter.writeResults("log2.csv", 0.00001, 5, log2, 0.1);
+        csvWriter.writeResults("log5.csv", 0.00001, 5, log5, 0.1);
+        csvWriter.writeResults("system.csv", -5, 5, function, 0.1);
     }
 }
