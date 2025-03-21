8fb68017969a299041f975c69b94bc8dd7c5ad02
Shir5 <Vova0120_black@icloud.com>
2025-03-21 10:05:18 +0300

    Cos impl fix

--- src/main/java/com/labwork/trigonometric/CosImpl.java
+++ src/main/java/com/labwork/trigonometric/CosImpl.java
@@ -8,35 +8,16 @@ import com.labwork.interfaces.FunctionInterface;
 public class CosImpl implements FunctionInterface {
 
     private static final MathContext MC = new MathContext(34, RoundingMode.HALF_EVEN);
-    private static final BigDecimal PI = new BigDecimal("3.141592653589793238462643383279", MC);
-    private static final BigDecimal TWO_PI = new BigDecimal("6.283185307179586476925286766559", MC);
+    // Значение π/2 с необходимой точностью
+    private static final BigDecimal HALF_PI = new BigDecimal("1.5707963267948966192313216916398", MC);
+
+    // Используем существующую реализацию синуса
+    private final SinImpl sinImpl = new SinImpl();
 
     @Override
     public BigDecimal calculate(final BigDecimal x, final BigDecimal delta) {
-        // нормализация
-        BigDecimal normX = x.remainder(TWO_PI);
-        if (normX.compareTo(PI) > 0) {
-            normX = normX.subtract(TWO_PI, MC);
-        } else if (normX.compareTo(PI.negate()) < 0) {
-            normX = normX.add(TWO_PI, MC);
-        }
-
-        // Разложение в ряд Тейлора для cos:
-        // cos(x) = 1 - x^2/2! + x^4/4! - x^6/6! + ...
-        BigDecimal term = BigDecimal.ONE; // Первый член ряда: 1
-        BigDecimal res = BigDecimal.ZERO;
-        int i = 0;
-
-        // Итеративное вычисление суммы ряда до достижения заданной точности delta
-        while (term.abs().compareTo(delta) > 0) {
-            res = res.add(term, MC);
-            i++;
-            // term = term * (-1) * x^2 / ((2*i - 1) * (2*i))
-            BigDecimal xSquared = normX.multiply(normX, MC);
-            BigDecimal denominator = new BigDecimal((2 * i - 1) * (2 * i), MC);
-            term = term.multiply(xSquared.negate(), MC)
-                    .divide(denominator, MC);
-        }
-        return res;
+        // Тождество: cos(x) = sin(π/2 - x)
+        BigDecimal arg = HALF_PI.subtract(x, MC);
+        return sinImpl.calculate(arg, delta);
     }
 }
